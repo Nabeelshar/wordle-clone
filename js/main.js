41e77c6f22d63e28750a81efd3b1e927e1be84a4
Nabeel Shar <96086329+Nabeelshar@users.noreply.github.com>
2022-03-16 16:52:21 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
   let guessedWords = [[]];
   let availableSpace = 1;
 
-  let word;
+  let word = 'lover';
   let guessedWordCount = 0;
 
   const keys = document.querySelectorAll(".keyboard-row button");
